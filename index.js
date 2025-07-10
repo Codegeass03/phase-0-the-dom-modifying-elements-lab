95df4022203f53ec880a62936deda59ad2793834
Vivian Cheruiyot <vivanchepchumba32@gmail.com>
2025-07-10 03:11:46 +0300

    completed DOM lab

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+const mainElem = document.querySelector('main#main');
+if (mainElem) {
+  mainElem.remove();
+}
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.innerHTML = 'Vee is the champion';
+document.body.appendChild(newHeader);
